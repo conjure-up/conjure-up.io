65e371f16efa96163ed19519eed5c97c3a16f536
Adam Stokes <adam.stokes@ubuntu.com>
2016-04-18 17:25:01 -0400

    update
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+JADE=node_modules/.bin/jade
+UGLIFY=node_modules/.bin/uglifyjs
+SCSS=node_modules/.bin/node-sass
+HTTPSERVE=node_modules/.bin/http-server
+
+if [ -d public ]; then
+  echo "cleaning public html"
+  rm -rf public/**/*.html
+  rm -rf public/{js,css}
+fi
+
+mkdir -p public/{js,css,images,files}
+
+# render templates
+echo "render templates"
+$JADE -H app -o public
+
+# process vendor js
+echo "render js"
+cat node_modules/jquery/dist/jquery.min.js \
+    node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js \
+    > public/js/app.js
+
+# process css
+echo "render scss"
+cat app/scss/app.scss | $SCSS --output-style compressed > public/css/app.css
+
+# copy assets
+echo "copying assets"
+rsync -az app/images public
+rsync -az node_modules/bootstrap-sass/assets/fonts/bootstrap/*.{ttf,woff,eot,svg,woff2} public/fonts
+rsync -az node_modules/font-awesome/fonts/*.{ttf,otf,eot,svg,woff,woff2} public/fonts
